bc3a13edfd7d2ec8e0591a20da385430238b7893
Amanda <amandaolivcampos@gmail.com>
2023-08-21 17:05:31 -0300

    Add Css

---
+++ src/app/estiloso/page.jsx
@@ -0,0 +1,7 @@
+export default function Estiloso() {
+  return (
+    <div className="bg-purple-300 text-white">
+      <h1>Estiloso Usando CSS Modulos</h1>
+    </div>
+  );
+}
--- src/pages/index.jsx
+++
@@ -1,7 +0,0 @@
-export default function Inicio() {
-  return (
-    <div>
-      <h1>Fundamentos de Next.js & React</h1>
-    </div>
-  );
-}
