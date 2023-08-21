ca29fea0b2a510f165a6dc2533c15fda712814f5
Amanda <amandaolivcampos@gmail.com>
2023-08-21 16:30:31 -0300

    Fundamentos JSX

--- src/pages/jsx.jsx
+++ src/pages/jsx.jsx
@@ -1,9 +1,12 @@
 export default function Jsx() {
   const titulo = <h1>Jsx e um conceito Central</h1>;
+  function subtitulo() {
+    return <h2>{"muito legal".toUpperCase()}</h2>;
+  }
   return (
     <div>
       {titulo}
-      <h2> {"muito legal".toUpperCase()}</h2>
+      {subtitulo()}
       <p>{JSON.stringify({ nome: "Joao", idade: 30 })}</p>
     </div>
   );
