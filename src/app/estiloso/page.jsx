854591b7d6eb3759647e6cda3ce5271b032875e4
Amanda <amandaolivcampos@gmail.com>
2023-08-23 14:42:52 -0300

    Navegando entre componentes

--- src/app/estiloso/page.jsx
+++ src/app/estiloso/page.jsx
@@ -1,6 +1,9 @@
+import Link from "next/link";
+
 export default function Estiloso() {
   return (
     <div className="bg-purple-300 text-white ">
+      <Link href="/inicio"> Voltar</Link>
       <h1>Estiloso Usando CSS Modulos</h1>
     </div>
   );
---
+++ src/app/inicio/page.jsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Inicio() {
+  return (
+    <div>
+      <Link href="/estiloso">Estiloso </Link>
+    </div>
+  );
+}
